aea63193b62d6f130aa1c7c38b3c861f39079ba7
Casimus <grzegorz.kazimierczak@wp.pl>
2025-09-25 16:02:16 +0200

    Animacja portalu

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -7,16 +7,39 @@ public class Portal : MonoBehaviour
     [SerializeField] Portal otherPortal;
     [SerializeField] Material material;
     [SerializeField] RenderTexture displayTexture;
+    [SerializeField] private GameObject door;
+    [SerializeField] private GameObject key;
+    [SerializeField] private Material keyMaterial;
+    [SerializeField] private KeyColor keyColor;
+    [SerializeField] private float rangeToOpen = 4f;
 
     public Camera myCamera;
 
     public Transform renderSurface;
     public Transform portalCollider;
 
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
     void Start()
     {
         renderSurface.GetComponent<Renderer>().material = material;
         otherPortal.myCamera.targetTexture = displayTexture;
+        door.GetComponent<Renderer>().material = keyMaterial;
+        key.GetComponent<Renderer>().material = keyMaterial;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && CanOpen() )
+        {
+            animator.SetTrigger("open");
+        }
     }
 
     public Transform GetOtherPortal()
@@ -24,7 +47,12 @@ public class Portal : MonoBehaviour
         return otherPortal.transform;
     }
 
+    private bool CanOpen()
+    {
+        Vector3 playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
 
+        return Vector3.Distance(playerPos, transform.position) < rangeToOpen;
+    }
 
 
 }
